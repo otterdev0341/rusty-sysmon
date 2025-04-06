b010f23c626e02b200b1576316eaac0cc39b1c6f
Otterdev_0341 <128258559+otterdev0341@users.noreply.github.com>
2025-04-06 23:05:58 +0700

    impl host utill
    get_os_name, get_kernel_version, get_os_version, get_host_name

--- rusty_sysmon/src/main.rs
+++ rusty_sysmon/src/main.rs
@@ -7,16 +7,27 @@
 
 use std::{thread, time::Duration};
 
-use rusty_sysmon::utility::ram::RamUtil;
+use rusty_sysmon::utility::{host::HostUtil, ram::RamUtil};
 
 
 fn main() {
     
-    for i in 1..=5 {
-        println!("Ram used : {:.2}", RamUtil::get_used_ram_gb());
-        thread::sleep(Duration::from_secs(i));
-    }
+    // for i in 1..=5 {
+    //     println!("Ram used : {:.2}", RamUtil::get_used_ram_gb());
+    //     thread::sleep(Duration::from_secs(i));
+    // }
 
-    println!("done after 5 secodes");
+    // println!("done after 5 secodes");
 
+    let host_name = HostUtil::get_os_name();
+    println!("{}", host_name);
+
+    let kernel_version = HostUtil::get_kernel_version();
+    println!("{}", kernel_version);
+
+    let os_version = HostUtil::get_os_version();
+    println!("{}", os_version);
+
+    let os_host_name = HostUtil::get_host_name();
+    println!("{}", os_host_name);
 }
--- rusty_sysmon/src/utility/host.rs
+++ rusty_sysmon/src/utility/host.rs
@@ -9,4 +9,32 @@ impl HostUtil {
         sys.refresh_all();
         sys
     }
+
+    pub fn get_os_name() -> String {
+        if let Some(data) = System::name() {
+            return data;
+        }
+        String::from("Unknown Host")
+    }
+
+    pub fn get_kernel_version() -> String {
+        if let Some(data) = System::kernel_version() {
+            return data;
+        }
+        String::from("Can't get kernel information")
+    }
+
+    pub fn get_os_version() -> String {
+        if let Some(data) = System::os_version() {
+            return data
+        }
+        String::from("can't get os version")
+    }
+
+    pub fn get_host_name() -> String {
+        if let Some(data) = System::host_name() {
+            return data
+        }
+        String::from("can't get os host name")
+    }
 }
