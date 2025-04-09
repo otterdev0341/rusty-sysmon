1bda790fa23803a8dbd37756547b67cedfbeadfa
Otterdev_0341 <128258559+otterdev0341@users.noreply.github.com>
2025-04-10 06:55:11 +0700

    impl ut

--- rusty_sysmon/src/main.rs
+++ rusty_sysmon/src/main.rs
@@ -7,6 +7,7 @@ slint::include_modules!();
 
 
 fn main() -> Result<(), Box<dyn Error>> {
+    // not error bec "rust-analyzer.procMacro.enable": true, in vs code
     let ui = AppEntry::new()?;
     ui.run()?;
 
