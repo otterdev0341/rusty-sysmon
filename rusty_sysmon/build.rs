0fec16e75fac843eed0bf447d153d9c08ba5ce9a
Otterdev_0341 <128258559+otterdev0341@users.noreply.github.com>
2025-04-11 09:07:36 +0700

    remove slint to prepare to use dioxus

--- rusty_sysmon/build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // This is the build script for the Slint UI framework.
-    slint_build::compile("ui/app.slint").expect("Slint build failed");
-    
-}
--- rusty_sysmon/src/main.rs
+++ rusty_sysmon/src/main.rs
@@ -1,21 +1,10 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-use std::error::Error;
-
-slint::include_modules!();
 
 
 
-fn main() -> Result<(), Box<dyn Error>> {
-    // not error bec "rust-analyzer.procMacro.enable": true, in vs code
-    let ui = AppEntry::new()?;
-    ui.run()?;
+pub fn main() {
 
-    Ok(())
+   println!("hello start migrate");
     
-   
-    
-
 }
 
         
